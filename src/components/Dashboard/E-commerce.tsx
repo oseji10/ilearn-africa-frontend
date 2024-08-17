0cdcf0b16d3bae1cc6a303dd1c463333e0454f72
Victor Oseji <vctroseji@gmail.com>
2024-08-17 11:55:31 +0100

    minor updates

--- src/components/Dashboard/E-commerce.tsx
+++ src/components/Dashboard/E-commerce.tsx
@@ -1,6 +1,6 @@
 "use client";
 import dynamic from "next/dynamic";
-import React from "react";
+import React, {useEffect, useState} from "react";
 import ChartOne from "../Charts/ChartOne";
 import ChartTwo from "../Charts/ChartTwo";
 import ChatCard from "../Chat/ChatCard";
@@ -17,21 +17,56 @@ const ChartThree = dynamic(() => import("@/components/Charts/ChartThree"), {
 });
 
 const ECommerce: React.FC = () => {
+const [clients, setClients] = useState("");
+const [applications_count, setApplications] = useState("");
+const [admitted_count, setAdmissions] = useState("");
+const [payments_count, setPayments] = useState("");
+
+  useEffect(() => {
+    const fetchStatistics = async () => {
+      const token = localStorage.getItem("token");
+      if (token) {
+        try {
+          const response = await fetch(
+            `${process.env.NEXT_PUBLIC_API_URL}/statistics`,
+            {
+              method: "GET",
+              headers: {
+                Authorization: `Bearer ${token}`,
+                "Content-Type": "application/json",
+              },
+            }
+          );
+          if (!response.ok) throw new Error("Network response was not ok");
+          const data = await response.json();
+          // console.log("HH", data)
+          setClients(data.clients); 
+          setApplications(data.applications_count); 
+          setAdmissions(data.admitted_count); 
+          setPayments(data.payments_count); 
+        } catch (error) {
+          console.error("Error fetching statistics:", error);
+        }
+      }
+    };
+
+    fetchStatistics();
+  }, []);
   return (
     <>
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5">
-        <CardDataStats title="Total Clients" total="3" 
+        <CardDataStats title="Total Clients" total={clients}
         rate="" 
         >
     <FontAwesomeIcon icon={faUsers} className="fill-primary dark:fill-white" size="lg"/>
         </CardDataStats>
-        <CardDataStats title="Total Applications" total="1" rate="" >
+        <CardDataStats title="Total Applications" total={applications_count} rate="" >
         <FontAwesomeIcon className="fill-primary dark:fill-white" icon={faMouse} />
         </CardDataStats>
-        <CardDataStats title="Total Payments" total="1" rate="" >
+        <CardDataStats title="Total Payments" total={payments_count} rate="" >
       <FontAwesomeIcon icon={faCartShopping} className="fill-primary dark:fill-white" />
         </CardDataStats>
-        <CardDataStats title="Certificates Issued" total="1" rate="" >
+        <CardDataStats title="Admitted" total={admitted_count} rate="" >
           <FontAwesomeIcon icon={faGraduationCap} className="fill-primary dark:fill-white" />
         </CardDataStats>
       </div>
