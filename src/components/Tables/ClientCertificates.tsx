ae3a926b457e34ab10e4a9bd33d9b380c67a6966
Victor Oseji <vctroseji@gmail.com>
2025-07-07 15:38:09 +0100

    update frontend

--- src/components/Tables/ClientCertificates.tsx
+++ src/components/Tables/ClientCertificates.tsx
@@ -3,7 +3,13 @@ import React, { useEffect, useState, useCallback } from "react";
 import axios from "axios";
 import DataTable from "react-data-table-component";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faDownload, faSpinner, faEye, faEnvelope, faEnvelopeOpen } from "@fortawesome/free-solid-svg-icons";
+import {
+  faDownload,
+  faSpinner,
+  faEye,
+  faEnvelope,
+  faEnvelopeOpen,
+} from "@fortawesome/free-solid-svg-icons";
 import { CSVLink } from "react-csv";
 import { useRouter } from "next/navigation";
 import { format } from "date-fns";
@@ -38,7 +44,7 @@ const ClientCertificates = () => {
             headers: {
               Authorization: `Bearer ${token}`,
             },
-          }
+          },
         );
         setAdmissions(response.data.certificates);
         setFilteredAdmissions(response.data.certificates); // Set initial filtered admissions
@@ -54,7 +60,8 @@ const ClientCertificates = () => {
   useEffect(() => {
     if (searchTerm) {
       const filtered = admissions.filter((admission) => {
-        const clientName = `${admission.clients?.title || ''} ${admission.clients?.firstname || ''} ${admission.clients?.othernames || ''} ${admission.clients?.surname || ''}`.toLowerCase();
+        const clientName =
+          `${admission.clients?.title || ""} ${admission.clients?.firstname || ""} ${admission.clients?.othernames || ""} ${admission.clients?.surname || ""}`.toLowerCase();
         const admissionNumber = admission.admission_number.toLowerCase();
         return (
           clientName.includes(searchTerm.toLowerCase()) ||
@@ -71,7 +78,9 @@ const ClientCertificates = () => {
   const handleApproval = async (event, approved) => {
     event.preventDefault();
     if (!confirmReceipt) {
-      alert("Please confirm that you authorize the issuance of this certificate.");
+      alert(
+        "Please confirm that you authorize the issuance of this certificate.",
+      );
       return;
     }
     setIsSubmitting(true);
@@ -88,12 +97,17 @@ const ClientCertificates = () => {
             Authorization: `Bearer ${token}`,
             "Content-Type": "application/json",
           },
-          body: JSON.stringify({ admission_number: selectedAdmission.admission_number, approved }),
-        }
+          body: JSON.stringify({
+            admission_number: selectedAdmission.admission_number,
+            approved,
+          }),
+        },
       );
 
       if (response.ok) {
-        alert(`This certificate has been ${approved ? "issued" : "disapproved for issuance"} successfully`);
+        alert(
+          `This certificate has been ${approved ? "issued" : "disapproved for issuance"} successfully`,
+        );
         setSelectedAdmission(null);
       } else {
         setError("There was an issue processing the certificate");
@@ -114,12 +128,14 @@ const ClientCertificates = () => {
     },
     {
       name: "Name",
-      selector: (row) => `${row.clients?.firstname || ''} ${row.clients?.surname || ''} ${row.clients?.othernames || ''}`,
+      selector: (row) =>
+        `${row.clients?.firstname || ""} ${row.clients?.surname || ""} ${row.clients?.othernames || ""}`,
       sortable: true,
     },
     {
       name: "Course Registered",
-      selector: (row) => `${row.payments?.courses?.course_id} - ${row.payments?.courses?.course_name}`,
+      selector: (row) =>
+        `${row.payments?.courses?.course_id} - ${row.payments?.courses?.course_name}`,
       sortable: true,
     },
     {
@@ -130,11 +146,15 @@ const ClientCertificates = () => {
             row.status === "COMPLETED"
               ? "bg-success text-success"
               : row.status === "ADMITTED"
-              ? "bg-danger text-danger"
-              : ""
+                ? "bg-danger text-danger"
+                : ""
           }`}
         >
-          {row.status === "ADMITTED" ? "NOT ISSUED" : row.status === "COMPLETED" ? "ISSUED" : "N/A"}
+          {row.status === "ADMITTED"
+            ? "NOT ISSUED"
+            : row.status === "COMPLETED"
+              ? "ISSUED"
+              : "N/A"}
         </p>
       ),
       sortable: true,
@@ -144,42 +164,40 @@ const ClientCertificates = () => {
       cell: (row) => (
         <div className="flex items-center space-x-3.5">
           <button
-          disabled={isDownloading}
-            className="px-4 py-2 bg-green-500 text-white rounded"
+            disabled={isDownloading}
+            className="rounded bg-green-500 px-4 py-2 text-white"
             onClick={() => downloadCertificate(row)}
           >
             {isDownloading && activeTransaction === row.admission_number ? (
-      <span>
-        Downloading... <FontAwesomeIcon icon={faSpinner} spin />
-      </span>
-    ) : (
-      <span>
-        <FontAwesomeIcon icon={faDownload} />&nbsp;
-         Download
-      </span>
-    )}
+              <span>
+                Downloading... <FontAwesomeIcon icon={faSpinner} spin />
+              </span>
+            ) : (
+              <span>
+                <FontAwesomeIcon icon={faDownload} />
+                &nbsp; Download
+              </span>
+            )}
             {/* <FontAwesomeIcon icon={faDownload} /> Download Certificate */}
           </button>
-
-&nbsp;
-
+          {/* &nbsp;
           <button
-          disabled={isEmailing}
-            className="px-4 py-2 bg-green-500 text-white rounded"
+            disabled={isEmailing}
+            className="rounded bg-green-500 px-4 py-2 text-white"
             onClick={() => emailCertificate(row)}
           >
             {isEmailing && activeTransaction === row.admission_number ? (
-      <span>
-        Sending... <FontAwesomeIcon icon={faSpinner} spin />
-      </span>
-    ) : (
-      <span>
-        <FontAwesomeIcon icon={faEnvelope} />&nbsp;
-         Email 
-      </span>
-    )}
-            {/* <FontAwesomeIcon icon={faDownload} /> Download Certificate */}
-          </button>
+              <span>
+                Sending... <FontAwesomeIcon icon={faSpinner} spin />
+              </span>
+            ) : (
+              <span>
+                <FontAwesomeIcon icon={faEnvelope} />
+                &nbsp; Email
+              </span>
+            )}
+           
+          </button> */}
         </div>
       ),
     },
@@ -193,20 +211,15 @@ const ClientCertificates = () => {
     return <p>Error: {error}</p>;
   }
 
-
-
-
-
-
-  const downloadCertificate= async (row) => {
+  const downloadCertificate = async (row) => {
     const admissionNumber = row.admission_number;
-  setIsDownloading(true)
-  setActiveTransaction(admissionNumber);
+    setIsDownloading(true);
+    setActiveTransaction(admissionNumber);
     const token = localStorage.getItem("token");
     if (!token) {
       throw new Error("No auth token found");
     }
-  
+
     try {
       const response = await fetch(
         `${process.env.NEXT_PUBLIC_API_URL}/certificates/client-certificate/download/${admissionNumber}`,
@@ -216,41 +229,46 @@ const ClientCertificates = () => {
             "Content-Type": "application/json",
             Authorization: `Bearer ${token}`,
           },
-          mode: 'cors',
-        }
+          mode: "cors",
+        },
       );
-  
+
       if (response.ok) {
         const blob = await response.blob();
         const url = window.URL.createObjectURL(blob);
         const link = document.createElement("a");
         link.href = url;
-        link.setAttribute("download", `iLearnCertificate-${admissionNumber}.pdf`);
+        link.setAttribute(
+          "download",
+          `iLearnCertificate-${admissionNumber}.pdf`,
+        );
         document.body.appendChild(link);
         link.click();
         link.parentNode.removeChild(link);
       } else {
-        console.error("Failed to download the admission letter", response.statusText);
+        console.error(
+          "Failed to download the admission letter",
+          response.statusText,
+        );
       }
     } catch (error) {
-      console.error("An error occurred while downloading the admission letter", error);
+      console.error(
+        "An error occurred while downloading the admission letter",
+        error,
+      );
     }
-    setIsDownloading(false)
+    setIsDownloading(false);
   };
 
-
-
-
-
-  const emailCertificate= async (row) => {
+  const emailCertificate = async (row) => {
     const admissionNumber = row.admission_number;
-  setIsEmailing(true)
-  setActiveTransaction(admissionNumber);
+    setIsEmailing(true);
+    setActiveTransaction(admissionNumber);
     const token = localStorage.getItem("token");
     if (!token) {
       throw new Error("No auth token found");
     }
-  
+
     try {
       const response = await fetch(
         `${process.env.NEXT_PUBLIC_API_URL}/certificates/client-certificate/email/${admissionNumber}`,
@@ -260,10 +278,10 @@ const ClientCertificates = () => {
             "Content-Type": "application/json",
             Authorization: `Bearer ${token}`,
           },
-          mode: 'cors',
-        }
+          mode: "cors",
+        },
       );
-  
+
       // if (response.ok) {
       //   const blob = await response.blob();
       //   const url = window.URL.createObjectURL(blob);
@@ -277,17 +295,18 @@ const ClientCertificates = () => {
       //   console.error("Failed to download the admission letter", response.statusText);
       // }
     } catch (error) {
-      console.error("An error occurred while emailing the admission letter", error);
+      console.error(
+        "An error occurred while emailing the admission letter",
+        error,
+      );
     }
-    setIsEmailing(false)
+    setIsEmailing(false);
   };
-
-
 
   return (
     <div>
       <CSVLink data={filteredAdmissions} filename={"certificates.csv"}>
-        <button className="mb-4 px-4 py-2 bg-blue-500 text-white rounded">
+        <button className="mb-4 rounded bg-blue-500 px-4 py-2 text-white">
           Download CSV
         </button>
       </CSVLink>
@@ -309,7 +328,7 @@ const ClientCertificates = () => {
             placeholder="Search Certificates"
             value={searchTerm}
             onChange={(e) => setSearchTerm(e.target.value)}
-            className="w-full px-4 py-2 border border-gray-300 rounded shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500"
+            className="border-gray-300 w-full rounded border px-4 py-2 shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500"
           />
         }
         subHeaderAlign="right"
@@ -324,25 +343,40 @@ const ClientCertificates = () => {
             <table width={"100%"}>
               <tbody>
                 <tr>
-                  <td width={"25%"}><strong>Client ID:</strong></td>
+                  <td width={"25%"}>
+                    <strong>Client ID:</strong>
+                  </td>
                   <td>{selectedAdmission.client_id}</td>
                 </tr>
                 <tr>
-                  <td><strong>Admission Number:</strong></td>
+                  <td>
+                    <strong>Admission Number:</strong>
+                  </td>
                   <td>{selectedAdmission.admission_number}</td>
                 </tr>
                 <tr>
-                  <td><strong>Name:</strong></td>
-                  <td>{selectedAdmission.clients?.firstname} {selectedAdmission.clients?.surname} {selectedAdmission.clients?.othernames}</td>
+                  <td>
+                    <strong>Name:</strong>
+                  </td>
+                  <td>
+                    {selectedAdmission.clients?.firstname}{" "}
+                    {selectedAdmission.clients?.surname}{" "}
+                    {selectedAdmission.clients?.othernames}
+                  </td>
                 </tr>
                 <tr>
-                  <td><strong>Course Registered:</strong></td>
-                  <td>{selectedAdmission.payments?.courses?.course_id} - {selectedAdmission.payments?.courses?.course_name}</td>
+                  <td>
+                    <strong>Course Registered:</strong>
+                  </td>
+                  <td>
+                    {selectedAdmission.payments?.courses?.course_id} -{" "}
+                    {selectedAdmission.payments?.courses?.course_name}
+                  </td>
                 </tr>
               </tbody>
             </table>
             <br />
-            <div className="flex items-center mb-4">
+            <div className="mb-4 flex items-center">
               <input
                 type="checkbox"
                 checked={confirmReceipt}
@@ -350,25 +384,29 @@ const ClientCertificates = () => {
                 className="mr-2"
                 required
               />
-              <label className="text-gray-700">By clicking this button, you confirm that this client is eligible to be issued this certificate under your authorization.</label>
+              <label className="text-gray-700">
+                By clicking this button, you confirm that this client is
+                eligible to be issued this certificate under your authorization.
+              </label>
             </div>
             <div className="flex justify-end">
               <button
                 onClick={(event) => handleApproval(event, true)}
                 disabled={isSubmitting}
-                className={`mr-2 px-4 py-2 bg-blue-500 text-white rounded ${isSubmitting ? "cursor-not-allowed opacity-50" : ""}`}
+                className={`mr-2 rounded bg-blue-500 px-4 py-2 text-white ${isSubmitting ? "cursor-not-allowed opacity-50" : ""}`}
               >
                 {isSubmitting ? (
                   <span>
-                    Please wait... <FontAwesomeIcon icon={faSpinner} spin className="mr-2" />
+                    Please wait...{" "}
+                    <FontAwesomeIcon icon={faSpinner} spin className="mr-2" />
                   </span>
                 ) : (
                   "Issue Certificate"
                 )}
               </button>
               <button
-              style={{background: 'red'}}
-                className="px-4 py-2 bg-red-500 text-black rounded"
+                style={{ background: "red" }}
+                className="bg-red-500 rounded px-4 py-2 text-black"
                 onClick={() => setSelectedAdmission(null)}
               >
                 Close
